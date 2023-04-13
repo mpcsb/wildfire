876b4cf232564dac79a645642e198049d8687255
Miguel <miguelcbatista@gmail.com>
2023-04-13 22:58:22 +0100

    .

--- testing/diffusion.py
+++ testing/diffusion.py
@@ -2,17 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numba
-import warnings
 from scipy.ndimage import gaussian_filter
-warnings.filterwarnings("ignore") 
 from time import time
-t0=time() 
+import warnings
+
+# Suppress warnings
+warnings.filterwarnings("ignore")
 
 
+# Define the reaction diffusion function
 @numba.jit
 def reaction_diffusion(u, D, dt, dx, dy, fire_threshold, fire_duration, burned):
-    u_x = (np.roll(u, -1, axis=1) - 2 * u + np.roll(u, 1, axis=1)) / dx**2
-    u_y = (np.roll(u, -1, axis=0) - 2 * u + np.roll(u, 1, axis=0)) / dy**2
+    # Calculate the Laplacian
+    u_x = (np.roll(u, -1, axis=1) - 2 * u + np.roll(u, 1, axis=1)) / dx ** 2
+    u_y = (np.roll(u, -1, axis=0) - 2 * u + np.roll(u, 1, axis=0)) / dy ** 2
+
+    # Update u with reaction and diffusion
     u += dt * D * (u_x + u_y)
 
     # Find locations that are above the fire_threshold and have not burned before
@@ -28,14 +33,23 @@ def reaction_diffusion(u, D, dt, dx, dy, fire_threshold, fire_duration, burned):
     return u, fire_counter, burned
 
 
+# Define the Gaussian function
 def gaussian(x, y, x0, y0, A, sigma):
-    return A * np.exp(-((x - x0)**2 + (y - y0)**2) / (2 * sigma**2))
+    return A * np.exp(-((x - x0) ** 2 + (y - y0) ** 2) / (2 * sigma ** 2))
 
-# Define the size of the domain
-width, height = 100, 100
 
-# Define the spatial resolution
-res_width, res_height = 500,500
+# Define simulation parameters
+width, height = 100, 100  # Define the size of the domain
+res_width, res_height = 400, 400  # Define the spatial resolution
+init_magnitude = 500  # Initial magnitude of the Gaussian heat source
+sigma = 2  # Standard deviation of the Gaussian heat source
+N = 2  # Maximum value of the medium
+down = 0.95  # Lower threshold for medium values
+up = 1.1  # Upper threshold for medium values
+b_width = 2  # Width of the boundary near the borders
+fire_threshold = 240  # Threshold value for fire
+fire_duration = 1  # Duration of fire
+frames = 1000  # Number of frames in animation
 
 # Calculate dx and dy based on the domain size and desired resolution
 dx = width / res_width
@@ -45,83 +59,64 @@ dy = height / res_height
 u = np.zeros((res_height, res_width))
 
 # Add initial conditions in each region
- 
-init_magnitude = 5
-sigma = 2
- 
-initial_conditions = [] 
+initial_conditions = []
 x, y = np.meshgrid(np.arange(res_width), np.arange(res_height))
-x0=50; y0=50; 
+x0 = 50
+y0 = 50
 initial_condition = gaussian(x, y, x0, y0, init_magnitude, sigma)
 u += initial_condition
 initial_conditions.append(initial_condition)
 
- 
-
-
 # Define the medium with random values between 0 and N
-N = 2
 D = np.random.rand(res_height, res_width) * N
 
 # Apply a Gaussian filter to smooth the medium
-D = gaussian_filter(D, sigma=3)
-# Set a threshold value R
-down = 0.95
-up = 1.1
+D = gaussian_filter(D, sigma=4)
+
 # Apply the rule to set values below R to zero
-D[D < down] = 0.0
-D[D > up] = 1
+D[D < 0.98] -= 0.7 
+D[D < 0.9] = 0
 
 # Create a boundary of width b_width near the borders
-b_width = 2
 D[:, :b_width] = 0  # Left border
 D[:, -b_width:] = 0  # Right border
 D[:b_width, :] = 0  # Top border
 D[-b_width:, :] = 0  # Bottom border
 
-# Plot the medium
-fig, ax = plt.subplots()
-im = ax.imshow(D, cmap='viridis', interpolation='nearest', animated=True)
-plt.colorbar(im)
-#plt.show()
-
-
-# Set the parameters for the simulation
-dt = 1 / (2 * (np.max(D) / dx**2 + np.max(D) / dy**2))
- 
-# Set the parameters for the simulation
-fire_threshold = 2.1  # Adjust the fire threshold as needed
-fire_duration = 10  # Adjust the fire duration as needed
-  
-
 # Initialize the burned array
 burned = np.zeros((res_height, res_width), dtype=bool)
 
 # Compute the simulation
-frames = 500
 u_states = [u.copy()]
 
+# Set the parameters for the simulation
+dt = 1 / (2 * (np.max(D) / dx ** 2 + np.max(D) / dy ** 2))
+
+# Run the simulation
 for frame in range(frames):
     u, fire_counter, burned = reaction_diffusion(u, D, dt, dx, dy, fire_threshold, fire_duration, burned)
 
     # Update heat sources based on fire_counter
     u[fire_counter > 0] += init_magnitude  # Add heat from burning trees
     fire_counter[fire_counter > 0] -= 1  # Decrease fire duration counter
-    if np.max(u) > 10:
-        print(frame)
+    u_states.append(u.copy())
 
-    u_states.append(u.copy()) 
+# Plot the medium
+fig1, ax1 = plt.subplots()
+im1 = ax1.imshow(D, cmap='viridis', interpolation='nearest', animated=True)
+plt.colorbar(im1)
 
-# Set up the plot
-fig, ax = plt.subplots()
-im = ax.imshow(u_states[0], cmap='viridis', interpolation='nearest', animated=True)
-plt.colorbar(im)
+# Set up the plot for the simulation
+fig2, ax2 = plt.subplots()
+im2 = ax2.imshow(u_states[0], cmap='viridis', interpolation='nearest', animated=True)
+plt.colorbar(im2)
 
+# Define the update function for the animation
 def update(frame):
-    im.set_array(u_states[frame])
-    return im,
+    im2.set_array(u_states[frame])
+    return im2,
 
-print(time()-t0)
-ani = animation.FuncAnimation(fig, update, frames=frames, interval=5, blit=True)
-print(time()-t0)
-plt.show() 
+# Create the animation
+ani = animation.FuncAnimation(fig2, update, frames=frames, interval=5, blit=True)
+
+plt.show()
